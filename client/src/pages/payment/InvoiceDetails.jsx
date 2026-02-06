166101388c551b8ec89969eb349caa5a10daf591
ck-shashank <ckshashank8541@gmail.com>
2026-02-06 14:43:28 +0530

    test purpose

--- client/src/pages/payment/InvoiceDetails.jsx
+++ client/src/pages/payment/InvoiceDetails.jsx
@@ -4,6 +4,7 @@ const InvoiceDetails = () => {
     return (
         <div className="invoice-details">
             <h1>Invoice Details</h1>
+            <h1>See the invoice details here</h1>
         </div>
     );
 };
