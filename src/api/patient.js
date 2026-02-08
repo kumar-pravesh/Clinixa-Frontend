cbf28d2de1c6ba6848788273688d8b83a1eb82e2
Pravesh935 <praveshkumar5502@gmail.com>
2026-02-08 11:22:15 +0530

    remove unnecessary file

--- src/api/patient.js
+++
@@ -1,4 +0,0 @@
-// import apiClient from './apiClient';
-
-// export const getPatients = () => apiClient.get('/patients');
-// export const createPatient = (patientData) => apiClient.post('/patients', patientData);
